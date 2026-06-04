dab2b7b8fece29795213bde80ed6741bb282a4a5
ahmadi008 <57325174+ahmadi008@users.noreply.github.com>
2026-06-04 20:37:40 +0430

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useState, useEffect, useRef } from 'react';
 import { ThemeProvider } from './components/ThemeProvider';
 import ScrollProgress from './components/ScrollProgress';
